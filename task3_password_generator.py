45cd27d200da01293b6ca1b6a4ccad5186c300be
tripathigovind2211 <tripathigovind2211@gmail.com>
2024-07-13 16:37:23 +0530

    Add password generator

---
+++ task3_password_generator.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    
+    return password
+
+def main():
+    length = int(input("Enter the desired length for the password: "))
+    password = generate_password(length)
+    
+    print(f"Generated password: {password}")
+
+if __name__ == "__main__":
+    main()
